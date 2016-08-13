48a275ee0ef8d898148ae28f9e79bf445e4d0a05
Héctor Villa <dnsdirmx@outlook.com>
2016-08-13 13:55:03 -0500

    agregada notificacion

--- app/src/main/java/mx/uv/varappmiento/services/SyncService.java
+++ app/src/main/java/mx/uv/varappmiento/services/SyncService.java
@@ -1,9 +1,16 @@
 package mx.uv.varappmiento.services;
 
 import android.app.IntentService;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
 import android.content.Intent;
+import android.support.v4.app.NotificationCompat;
+import android.support.v4.app.TaskStackBuilder;
 
+import mx.uv.varappmiento.R;
 import mx.uv.varappmiento.controllers.OrdenesController;
+import mx.uv.varappmiento.views.PrincipalActivity;
 
 /**
  * Created by willo on 10/08/2016.
@@ -18,6 +25,38 @@ public class SyncService extends IntentService{
     protected void onHandleIntent(Intent intent) {
         //metodo de sincnonizacion
         OrdenesController.getInstance().syncOrdenes();
+
+
+
+
+
+        NotificationCompat.Builder mBuilder =
+                new NotificationCompat.Builder(this)
+                        .setSmallIcon(R.drawable.ic_add_a_photo_white_48dp)
+                        .setContentTitle("My notification")
+                        .setContentText("Hello World!");
+// Creates an explicit intent for an Activity in your app
+        Intent resultIntent = new Intent(this, PrincipalActivity.class);
+
+// The stack builder object will contain an artificial back stack for the
+// started Activity.
+// This ensures that navigating backward from the Activity leads out of
+// your application to the Home screen.
+        TaskStackBuilder stackBuilder = TaskStackBuilder.create(this);
+// Adds the back stack for the Intent (but not the Intent itself)
+        stackBuilder.addParentStack(PrincipalActivity.class);
+// Adds the Intent that starts the Activity to the top of the stack
+        stackBuilder.addNextIntent(resultIntent);
+        PendingIntent resultPendingIntent =
+                stackBuilder.getPendingIntent(
+                        0,
+                        PendingIntent.FLAG_UPDATE_CURRENT
+                );
+        mBuilder.setContentIntent(resultPendingIntent);
+        NotificationManager mNotificationManager =
+                (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+// mId allows you to update the notification later on.
+        mNotificationManager.notify(1, mBuilder.build());
 
     }
 }
